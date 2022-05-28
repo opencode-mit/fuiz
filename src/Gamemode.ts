092432b0aed3db2391ee35959760f6625cbe1319
Khaleel Al-Adhami <adhami@mit.edu>
2022-05-28 20:39:31 +0300

    finished basic Quiz

--- src/Gamemode.ts
+++ src/Gamemode.ts
@@ -17,23 +17,52 @@ export interface Gamemode {
 class Quiz implements Gamemode {
 
     private readonly toBeResolved: Array<{ resolved: boolean, callback: () => void }> = [];
-
     private readonly answers: Array<Array<Answer>> = [];
-
     private currentQuestion = -1;
+    private acceptingResponses = false;
 
     public constructor(
-        config: GameConfig,
+        private readonly config: GameConfig,
         private readonly announceCallback: (action: Action) => void
-    ) { }
+    ) {
+        this.toBeResolved.push({ resolved: false, callback: this.startGame })
+    }
 
     public submitAnswer(questionID: number, playerID: string, answerID: number): void {
         const questionAnswers = this.answers[questionID];
-        if (questionAnswers !== undefined && this.currentQuestion === questionID) {
+        if (this.acceptingResponses && questionAnswers !== undefined && this.currentQuestion === questionID) {
             questionAnswers.push({ playerID: playerID, answerID: answerID, timeSubmitted: Date.now() });
         } else {
             // do something D:
         }
+    }
+
+    private startGame() {
+        this.announceNextQuestion();
+    }
+
+    private announceNextQuestion() {
+        this.currentQuestion++;
+        const question = this.config.questions[this.currentQuestion];
+        if (question === undefined) return;
+        this.toBeResolved.push({ resolved: false, callback: this.stopQuestion });
+        setTimeout(() => this.resolveAction(this.toBeResolved.length - 1), this.config.delay);
+        this.acceptingResponses = true;
+        this.announceCallback({
+            type: 'question',
+            content: question.content,
+            answers: question.answers.map((answer) => {
+                return { content: answer.content };
+            })
+        });
+    }
+
+    private stopQuestion() {
+        this.toBeResolved.push({ resolved: false, callback: this.announceNextQuestion });
+        this.acceptingResponses = false;
+        this.announceCallback({
+            type: 'leaderboard'
+        });
     }
 
     public resolveAction(actionID: number): void {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-export type GameConfig = JSON;
-export type Action = JSON;
+export type GameConfig = {
+    gamemode: string,
+    delay: number,
+    questions: Array<{
+        content: string,
+        answers: Array<{
+            content: string,
+            correct: boolean
+        }>
+    }> 
+};
+export type Action = {
+    type: 'question',
+    content: string,
+    answers: Array<{
+        content: string
+    }>
+} | {
+    type: 'leaderboard'
+};
 export type SessionID = string;
 export type PlayerID = string;
