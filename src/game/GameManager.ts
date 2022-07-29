4286950b63a1048fcb8f53bcc4abc618dec01ce8
Khaleel Al-Adhami <adhami@mit.edu>
2022-07-29 19:05:57 +0300

    added documentation for gamemanager functions

--- src/game/GameManager.ts
+++ src/game/GameManager.ts
@@ -40,6 +40,12 @@ export class GameManager {
         this.socketManager = new ServerSocket(server, (sessionID, message) => this.receiveResponse(sessionID, message));
     }
 
+    /**
+     * Registers a new game with given configuration
+     * 
+     * @param config Game Configuration
+     * @returns the newly assigned SessionID and the secret Hash
+     */
     public registerHost(config: GameConfig): { sessionID: SessionID, token: Hash } {
         const nextSessionID = getRandomSessionID(Array.from(this.mapping.keys()));
         const token = getRandomHash();
@@ -49,11 +55,25 @@ export class GameManager {
         return { sessionID: nextSessionID, token: token };
     }
 
+    /**
+     * Subscribes the listener to announcment of the given game
+     * 
+     * @param sessionID the sessionID of the game
+     * @param socketID the socketID of the listener
+     */
     public registerSocket(sessionID: SessionID, socketID: SocketID): void {
         assert(this.socketManager !== undefined);
         this.socketManager.addToSession(socketID, sessionID);
     }
 
+    /**
+     * Registers a player as a listener for a given game under a name
+     * 
+     * @param socketID SocketID of the player for announcing actions
+     * @param sessionID the SessionID of the game
+     * @param playerID the player's ID
+     * @returns the verificaion hash and last action announced by the the game
+     */
     public registerPlayer(socketID: SocketID, sessionID: SessionID, playerID: PlayerID): { token: Hash, lastAction: Action } {
         const game = this.mapping.get(sessionID);
         if (game === undefined) throw new JoiningError("Game PID not found");
@@ -65,11 +85,24 @@ export class GameManager {
         return { token: playerToken, lastAction: game.game.lastAction() };
     }
 
+    /**
+     * Announces an action to the watchers of the given SessionID
+     * 
+     * @param sessionID the SessionID of the game
+     * @param action the action to be announced
+     */
     private announceAction(sessionID: SessionID, action: Action): void {
         assert(this.socketManager !== undefined);
         this.socketManager.broadcast(sessionID, action);
     }
 
+    /**
+     * Resolves an action in a given game. Might cause the game to continue.
+     * 
+     * @param sessionID SessionID of the game
+     * @param actionID the number of the action
+     * @param token the secret hash for the given game
+     */
     public resolveAction(sessionID: SessionID, actionID: number, token: Hash): void {
         const game = this.mapping.get(sessionID);
         if (game === undefined) throw new AuthenticationError("SessionID was not found");
@@ -77,6 +110,15 @@ export class GameManager {
         game.game.resolveAction(actionID);
     }
 
+    /**
+     * Handles an answer by a player for a question in a given game
+     * 
+     * @param sessionID SessionID of the game
+     * @param playerID PlayerID of the player who submitted the answer
+     * @param playerToken Player's Hash for verifican
+     * @param questionID Number of the question
+     * @param answerID Number of the answer
+     */
     public submitAnswer(sessionID: SessionID, playerID: PlayerID, playerToken: Hash, questionID: number, answerID: number): void {
         const game = this.mapping.get(sessionID);
         if (game === undefined) throw new AuthenticationError("SessionID was not found");
@@ -86,6 +128,12 @@ export class GameManager {
         game.game.submitAnswer(questionID, playerID, answerID);
     }
 
+    /**
+     * handles replies from the websockets
+     * 
+     * @param sessionID given session ID of the game
+     * @param message the reply from the players
+     */
     public receiveResponse(sessionID: SessionID, message: GameResponse): void {
         switch (message.type) {
             case GameResponseType.Answer:
